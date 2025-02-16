ba3845d25630332951502ae37f1a8669527d7ec9
caio-zarrari <zarraric@gmail.com>
2025-02-16 13:16:28 +0000

    ola estou aprendendo java

---
+++ MinhaClasse.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MinhaClasse {
+  public static void main(String[]aStrings){
+   String MeuNome = ".......";
+ Scanner scanner = new Scanner(System.in);
+        System.out.print("               ola         ");
+        System.out.print("                                                                                como posso ajudar você hoje =]          ");
+
+        String comando = scanner.nextLine();  
+        if (comando.equalsIgnoreCase("quem e voce")){
+            System.out.println(" ksksksksksksks      desculpa,onde estâo meu bons modos           sou..........  hmmm não tenho nome ainda não estou finalisado ainda");
+            System.out.println(" deseja me nomear ...............digite: nomear");}
+            if (comando.equalsIgnoreCase("nomear")){
+                System.out.println("ok algum nome em mente?");
+                System.out.println("Muito bem! Meu novo nome é: " + MeuNome);
+            
+                
+               }
+           
+         };
+            
+          
+         
+          
+          
+
+        }
+    
